89991eb191f6e8adf22bffade360ddaa8d3cb5ee
Newz CJ <newz.cj@gmail.com>
2019-12-27 15:07:46 +0700

    addAll

---
+++ Test2_SQLite/Test2_SQLite/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Test2_SQLite
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ArrayList arrayList = new ArrayList();
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataAccess.InitializeDatabase();
+            DataAccess.AddData("aaaa");
+            
+            foreach(string data in DataAccess.GetData())
+            {
+                MessageBox.Show(data);
+            }
+        }
+    }
+}
